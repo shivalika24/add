289d4d3c5fcd0aa1046dba5d4c2d16920717da5f
shivalika24 <99875461+shivalika24@users.noreply.github.com>
2022-02-23 21:30:42 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,17 @@
+package abc;
+import java.util.Scanner;
+public class Typecasting {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the num value");
+		double num=sc.nextDouble();
+		System.out.println("double value is:"+num);
+		{
+			int data=(int)num;
+			System.out.println("integer value is:"+(int)num);
+		}
+		
+	}
+
+}
